7759999382f43fef82e2bc9a41229c1afd7c7208
Katsuki Oike <katsuki.oike@gmail.com>
2023-02-05 15:16:33 -0600

    delete unnecessary comments

--- src/merge-array2.ts
+++ src/merge-array2.ts
@@ -19,27 +19,6 @@ function merge02(arr1: Array<number>, arr2: Array<number>): Array<number> {
     count += 1;
   }
 
-  // if (arr1.length < arr2.length) {
-  //   tempSize = arr1.length;
-  // } else {
-  //   tempSize = arr2.length;
-  // }
-
-  // for (let i = 0; i < tempSize; i += 1) {
-  //   mergedArr.push(arr1[i]);
-  //   mergedArr.push(arr2[i]);
-  // }
-
-  // if (arr1.length < arr2.length) {
-  //   for (let i = arr1.length; i < arr2.length; i += 1) {
-  //     mergedArr.push(arr2[i]);
-  //   }
-  // } else {
-  //   for (let i = arr2.length; i < arr1.length; i += 1) {
-  //     mergedArr.push(arr1[i]);
-  //   }
-  // }
-
   return mergedArr;
 }
 
